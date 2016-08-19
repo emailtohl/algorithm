a83a04194c936e626eb906507b1885ccfa545877
helei <emailtohl@163.com>
2016-08-19 23:12:25 +0800

    添加注释，不过KMP的思维模式的确复杂，注释似乎不能解释得清晰

--- src/main/java/com/github/emailtohl/algorithm/character/Kmp.java
+++ src/main/java/com/github/emailtohl/algorithm/character/Kmp.java
@@ -6,10 +6,25 @@ package com.github.emailtohl.algorithm.character;
  * @author HeLei
  */
 public class Kmp {
-
+	/**
+	 * 通过模式串计算出next数组
+	 * 数组中的值表示当匹配失败后，模式串中的该值将作为下次匹配的起点。
+	 * 
+	 * 例如模式串“abababb”的next数组是：[-1,0,0,1,2,3,4]，这表示：
+	 * 当第0位匹配失败了，将序号为-1用于下次匹配，当然-1很特殊，这说明文本字符串和模式串都向下一格
+	 * 当第1位匹配失败了，则将模式串的第0位与文本串的当前位置进行重新匹配
+	 * 当第2位匹配失败了，则将模式串的第0位与文本串的当前位置进行重新匹配
+	 * 当第3位匹配失败了，则将模式串的第1位与文本串的当前位置进行重新匹配，可以看出模式串的第0位不需要匹配了
+	 * 当第4位匹配失败了，则将模式串的第2位与文本串的当前位置进行重新匹配，可以看出模式串的第0,1位不需要匹配了
+	 * 当第5位匹配失败了，则将模式串的第3位与文本串的当前位置进行重新匹配，可以看出模式串的第0,1,2位不需要匹配了
+	 * 当第6位匹配失败了，则将模式串的第4位与文本串的当前位置进行重新匹配，可以看出模式串的第0,1,2,3位不需要匹配了
+	 * 
+	 * 可以看出他们存在交叉的情况
+	 * 
+	 * @param pattern
+	 * @return 
+	 */
 	int[] getNextArray(String pattern) {
-		if (pattern == null)
-			return null;
 		int i, j, N;
 		N = pattern.length();
 		int[] next = new int[N];
@@ -29,11 +44,17 @@ public class Kmp {
 		}
 		return next;
 	}
-
+	/**
+	 * 返回匹配的位置
+	 * @param txt 查找的字符串
+	 * @param pattern 模式串
+	 * @param startPost 查找位置
+	 * @return 若返回-1表示未找到匹配的位置
+	 */
 	public int search(String txt, String pattern, int startPost) {
-		if (txt == null && pattern == null && startPost < 0)
-			return -1;
-		int i, j, tlen, plen;
+		if (startPost < 0)
+			startPost = 0;
+		int i, j, tlen, plen;// i跟踪文本串，j跟踪模式串
 		j = -1;// next[0]的值是-1开始的，每当j等于-1时，模式串指针重置为0
 		i = startPost - 1;// 而此时的字符串txt的指针也会后移一位，所以初始值先减1
 		tlen = txt.length();
@@ -44,6 +65,7 @@ public class Kmp {
 				i++;
 				j++;
 			} else
+				// 当匹配失败时，i不会回退，而是调整j指针，在next数组查询从哪个地方开始重新匹配
 				j = next[j];
 		}
 		if (j >= plen)
